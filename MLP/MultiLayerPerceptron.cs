73c53442efb5f8a99925ca0d02534a48bb23857b
Dominik Messinger <domessin@microsoft.com>
2016-08-01 18:33:15 -0700

    Backprpagation MLP with single byte input and output, layer and neuron count configurable

---
+++ MLP/MultiLayerPerceptron.cs
@@ -0,0 +1,218 @@
+﻿/* Copyright Dominik Messinger, dominik.messinger@gmail.com */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MLP
+{
+    // n-layer MLP:
+    // fully connected with max 8 input and max 8 output neurons
+    class MultiLayerPerceptron
+    {
+
+        private const double learningRate = 0.8;
+        internal int MaxIterations { get; set; }
+        internal double MaxTotalError { get; set; }
+        private bool allowPrint = true;
+
+        private readonly int[] neuronsInLayer = {8, 12, 1};
+        private double[] weights;
+        private double[,] results;
+        private double totalError;
+
+        internal MultiLayerPerceptron()
+        {
+            int connections = getNumberOfConnections();
+            weights = new double[connections];
+            results = new double[neuronsInLayer.Length, neuronsInLayer.Max()];
+        }
+
+        private int getNumberOfConnections()
+        {
+            int connections = 0;
+            for (int i = 0; i < neuronsInLayer.Length - 1; i++)
+                connections += neuronsInLayer[i] * neuronsInLayer[i + 1];
+            return connections;
+        }
+
+        private bool Terminate(int? iteration, double? totalError)
+        {
+            if (MaxTotalError == 0 && MaxIterations == 0)
+                return true;
+            return totalError <= MaxTotalError || iteration >= MaxIterations;
+        }
+
+        internal void Train(byte[] inputs, byte[] desiredOutputs)
+        {
+            if(inputs.Length != desiredOutputs.Length)
+                throw new ArgumentException();
+
+            Console.WriteLine("=== Learning ... ===");
+            int iteration = 0;
+            do
+            {
+                totalError = 0;
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    Compute(inputs[i]);
+                    var meanSqError = BackPropagate(desiredOutputs[i]);
+                    totalError += meanSqError / inputs.Length;
+                }
+                Console.WriteLine("{0}: {1}", iteration, totalError);
+            }
+            while (!Terminate(++iteration, totalError));
+        }
+
+        internal byte Compute(byte input)
+        {
+            InitResults(input);
+            for (int neuron = 0; neuron < neuronsInLayer[0]; neuron++)
+            {
+                results[0, neuron] = (double) ((input >> neuron) & 1);
+            }
+            for (int layer = 1; layer < neuronsInLayer.Length; layer++)
+            {
+                for (int neuron = 0; neuron < neuronsInLayer[layer]; neuron++)
+                {
+                    for (int neuronPrev = 0; neuronPrev < neuronsInLayer[layer - 1]; neuronPrev++)
+                    {
+                        double x = results[layer - 1, neuronPrev] * getWeight(layer - 1, neuronPrev, neuron);
+                        results[layer, neuron] += x;
+                    }
+                    results[layer, neuron] = activate(results[layer, neuron]);
+                }
+            }
+            return byteOutputFromLastLayer();
+        }
+
+        // works only for max 8 output neurons
+        private byte byteOutputFromLastLayer()
+        {
+            int output = 0;
+            int lastLayer = neuronsInLayer.Length - 1;
+            for (int neuron = neuronsInLayer[lastLayer] - 1; neuron >= 0; neuron--)
+            {
+                output <<= 1;
+                output += (int) Math.Round(results[lastLayer, neuron]); // round to 0 or 1
+            }
+            return (byte) output;
+        }
+
+        internal double BackPropagate(byte desiredOutput)
+        {
+            double[,] errors = new double[neuronsInLayer.Length, neuronsInLayer.Max()];
+
+            int lastLayer = neuronsInLayer.Length - 1;
+            double meanSquaredError = 0;
+            for (int layer = lastLayer; layer > 0; layer--)
+            {
+                for (int neuron = 0; neuron < neuronsInLayer[layer]; neuron++)
+                {
+                    // compute errors
+                    double neuronOutput = results[layer, neuron];
+                    double phi = neuronOutput * (1 - neuronOutput);
+                    if(layer == lastLayer)
+                    {
+                        double delta = (double)((desiredOutput >> neuron) & 1) - neuronOutput;
+                        errors[layer, neuron] = phi * delta;
+                        meanSquaredError += Math.Pow(phi * delta, 2);
+                    }
+                    else
+                    {
+                        double errorWeightSum = 0;
+                        for (int neuronNext = 0; neuronNext < neuronsInLayer[layer + 1]; neuronNext++)
+                            errorWeightSum += errors[layer + 1, neuronNext] * getWeight(layer, neuron, neuronNext);
+                        errors[layer, neuron] = phi * errorWeightSum;
+                    }
+                    // adjust weights
+                    for (int neuronPrev = 0; neuronPrev < neuronsInLayer[layer - 1]; neuronPrev++)
+                    {
+                        double newWeight = getWeight(layer - 1, neuronPrev, neuron) + learningRate * errors[layer, neuron] * results[layer - 1, neuronPrev];
+                        setWeight(layer - 1, neuronPrev, neuron, newWeight);
+                    }
+                }
+            }
+            meanSquaredError /= (neuronsInLayer[lastLayer] + 1);
+            return meanSquaredError;
+        }
+
+
+        private double getWeight(int layer, int neuron, int neuronNext)
+        {
+            if (layer >= neuronsInLayer.Length - 1)
+                throw new ArgumentException();
+
+            int index = getWeightIndex(layer, neuron, neuronNext);
+            return weights[index];
+        }
+
+        private void setWeight(int layer, int neuron, int neuronNext, double value)
+        {
+            if (layer >= neuronsInLayer.Length - 1)
+                throw new ArgumentException();
+
+            int index = getWeightIndex(layer, neuron, neuronNext);
+            weights[index] = value;
+        }
+
+        private int getWeightIndex(int layer, int neuron, int neuronNext)
+        {
+            int layerOffset = 0;
+            for (int l = 0; l < layer; l++)
+                layerOffset += neuronsInLayer[l];
+            int index = layerOffset + neuron * neuronsInLayer[layer + 1] + neuronNext;
+            return index;
+        }
+
+        private double activate(double p)
+        {
+            // sigmoid
+            return 1 / (1 + Math.Pow(Math.E, -p));
+        }
+
+        private void InitResults(byte xs)
+        {
+            for (int layer = 0; layer < neuronsInLayer.Length; layer++)
+            {
+                for (int neuron = 0; neuron < neuronsInLayer[layer]; neuron++)
+                {
+                    results[layer, neuron] = .0f ;
+                }
+            }
+        }
+
+        internal void RandomizeWeights()
+        {
+            totalError = 0;
+            Random rand = new Random();
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] = rand.NextDouble() * 2 - 1;
+            }
+        }
+
+        private void printResults()
+        {
+            if (!allowPrint)
+                return;
+            Console.WriteLine("=== RESULTS ===");
+            printMatrix(results);
+        }
+
+        private void printMatrix(double[,] matrix)
+        {
+            for (int layer = 0; layer < neuronsInLayer.Length; layer++)
+            {
+                for (int neuron = 0; neuron < neuronsInLayer[layer]; neuron++)
+                {
+                    Console.Write(matrix[layer, neuron].ToString("F2"));
+                    if (neuron != neuronsInLayer[layer] - 1)
+                        Console.Write(", ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ MLP/Program.cs
@@ -0,0 +1,102 @@
+﻿/* Copyright Dominik Messinger, dominik.messinger@gmail.com */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiLayerPerceptron mlp = new MultiLayerPerceptron();
+
+            Condition cond = IsPrime;
+            TargetFunction targetFunc = b => BoolToByte(cond(b));
+
+            byte[] inputs;
+            GenerateByteInputs(out inputs, 20, 120);
+            byte[] desiredOutputs;
+            GenerateByteOutputs(out desiredOutputs, targetFunc, 20, 120);
+
+            mlp.RandomizeWeights();
+            mlp.MaxTotalError = 0.00001;
+            mlp.MaxIterations = 1000;
+
+            mlp.Train(inputs, desiredOutputs);
+
+            Test(mlp, 100, targetFunc);
+            Console.ReadKey();
+        }
+
+        delegate bool Condition(byte b);
+        delegate byte TargetFunction(byte b);
+
+        private static Condition DivisibleBy3 = b => ((int) b) % 3 == 0;
+        private static Condition Less20OrGreater90 = b => (b < 20 || b > 90);
+        private static Condition Equals42 = b => (b == 42);
+        private static Condition Equals23Or42 = b => (b == 23 || b == 42);
+        private static Condition ContainsCipher2 = b => (b.ToString().Contains("2"));
+        private static Condition IsOdd = b => ((int)b) % 2 == 1;
+        private static Condition IsPrime = b => Primes.Contains((int) b);
+
+        private static readonly List<int> Primes = new List<int>() {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241}; 
+
+        private static bool ByteToBool(byte b)
+        {
+            if (b > 1)
+                throw new ArgumentException();
+            // lsb
+            return (b & 1) == 1;
+        }
+
+        private static byte BoolToByte(bool b)
+        {
+            return b ? (byte) 1 : (byte) 0;
+        }
+
+        private static void Test(MultiLayerPerceptron mlp, int n, TargetFunction func)
+        {
+            Console.WriteLine("\n======= Test =======");
+            Random rand = new Random();
+            int matches = 0;
+            for(int i = 0; i < n; i++)
+            {
+                int x = rand.Next(256);
+                byte bx = (byte) x;
+                byte by = mlp.Compute(bx);
+                byte correctY = func(bx);
+                if(correctY == by)
+                {
+                    matches++;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("{0}: {1} = {2}", x, correctY, by);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0}: {1} != {2}", x, correctY, by);
+                }
+            }
+            Console.ResetColor();
+            Console.WriteLine("\n{0} correct out of {1}", matches, n);
+        }
+
+        private static void GenerateByteInputs(out byte[] inputs, int start = 0, int end = 256)
+        {
+            inputs = new byte[end - start];
+            for (int i = start; i < end; i++)
+                inputs[i - start] = byte.Parse(i.ToString()); // BitConverter and endianness check would also work, but we have only one byte anyway
+        }
+
+        private static void GenerateByteOutputs(out byte[] desiredOutputs, TargetFunction func, int start = 0, int end = 256)
+        {
+            desiredOutputs = new byte[end - start];
+            for (int i = start; i < end; i++)
+                desiredOutputs[i - start] = func((byte) i);
+        }
+    }
+}
